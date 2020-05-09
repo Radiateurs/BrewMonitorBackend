04881fa0d5a9c4b5e3aca021e7fb3528d92bcf77
Radiateurs <pierre-marie.danieau@epitech.eu>
2020-05-09 15:43:28 -0700

    Updating routers

--- src/routes.ts
+++ src/routes.ts
@@ -9,9 +9,12 @@ import ReceipeIngredientRouter from "./router/ReceipeIngredient.router"
 
 const IndexRouter = Router();
 
-IndexRouter.use("/users/", UserRouter);
-IndexRouter.use("/auth/", AuthRouter);
-IndexRouter.use("/brewing/", AuthRouter);
-IndexRouter.use("/auth/", AuthRouter);
+IndexRouter.use("/users", UserRouter);
+IndexRouter.use("/auth", AuthRouter);
+IndexRouter.use("/brewing", BrewingRouter);
+IndexRouter.use("/fermentor", FermentorRouter);
+IndexRouter.use("/ingredient", IngredientRouter);
+IndexRouter.use("/receipe", ReceipeRouter);
+IndexRouter.use("/receipeingredient", ReceipeIngredientRouter);
 
 export default (IndexRouter);
