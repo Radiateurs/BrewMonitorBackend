f3dae838252f49029d06922b78342a4efd4e521b
Radiateurs <pierre-marie.danieau@epitech.eu>
2020-08-04 21:31:32 -0700

    Adding configuration files for passeport (Auth), typeorm (DB), typescript config and serve

--- src/router/Auth.router.ts
+++ src/router/Auth.router.ts
@@ -4,7 +4,6 @@ import { AuthController } from "../controller/Auth.controller"
 const AuthRouter = Router();
 
 AuthRouter.get("/login", AuthController.connect);
-AuthRouter.post("/signin", AuthController.signin);
 AuthRouter.get("/logoff", AuthController.disconnect);
 
 export default AuthRouter;
